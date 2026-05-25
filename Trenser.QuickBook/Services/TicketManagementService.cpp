11544a8df1e4bd6d4e8e5b335f2f91107a01a49a
Ajmal Jalaludeen <ajmal.jalaludeen@trenser.com>
2026-05-25 14:41:56 +0530

    Added Function Headers

--- Trenser.QuickBook/Services/TicketManagementService.cpp
+++ Trenser.QuickBook/Services/TicketManagementService.cpp
@@ -1,14 +1,37 @@
+/*
+ * File: TicketManagementService.h
+ * Description: Declares the TicketManagementService class, which provides
+ *              functionality for managing tickets within the system.
+ *              This service will handle operations such as issuing,
+ *              validating, retrieving, and canceling tickets for shows.
+ * Author: Trenser
+ * Created: 20 May 2026
+ */
 #include <iomanip>
 #include <sstream>
 #include "TicketManagementService.h"
 #include "Factory.h"
 #include "Enums.h"
 
+ /*
+ * Function Name : default constructor
+ * Description   : Constructor that initializes the TicketManagementService
+ *                 by retrieving the singleton instance of DataStore.
+ * Parameters    : None
+ * Return Type   : None
+ */
 TicketManagementService::TicketManagementService()
 	: m_dataStore(DataStore::getInstance())
 {
 }
 
+/*
+* Function Name : TicketManagementService::generateTicketId
+* Description   : Generates a unique ticket ID based on the current number
+*                 of tickets in the datastore. The ID is formatted as "USXXX".
+* Parameters    : None
+* Return Type   : const std::string
+*/
 const std::string TicketManagementService::generateTicketId()
 {
 	const std::map<std::string, Ticket*>& tickets = m_dataStore.getTickets();
@@ -18,6 +41,15 @@ const std::string TicketManagementService::generateTicketId()
 	return buffer.str();
 }
 
+/*
+* Function Name : TicketManagementService::generateTicket
+* Description   : Creates a new ticket if the payment is valid and adds it
+*                 to the datastore. Returns the process status.
+* Parameters    :
+*                  payment   - Pointer to the Payment object associated with the ticket
+*                  customer  - Pointer to the User object representing the customer
+* Return Type   : Enums::ProcessStatus
+*/
 Enums::ProcessStatus TicketManagementService::generateTicket(Payment* payment, User* customer)
 {
 	if (!payment)
