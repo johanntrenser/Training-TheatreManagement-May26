86b3c66a034ddd3cdeca229af049c69f36466bbf
Ajmal Jalaludeen <ajmal.jalaludeen@trenser.com>
2026-05-25 16:34:55 +0530

    Resloved reviewer comments

--- Trenser.QuickBook/Views/UserInterface.h
+++ Trenser.QuickBook/Views/UserInterface.h
@@ -30,5 +30,5 @@ public:
 	void viewActiveTicketDetails();
 	void viewAllTickets();
 	void viewTicketHistory();
-	void viewTicketDetails(const std::vector<const Ticket*>);
+	void viewTicketDetails(const std::vector<const Ticket*>& tickets);
 };
